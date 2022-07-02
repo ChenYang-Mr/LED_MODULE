c8cd8acf9802aee9f6cf1cd79db98ff9a6cee2ae
chen_yang <2584623834@qq.com>
2022-07-02 16:21:58 +0800

    32

---
+++ Debug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../appl.c 
+
+OBJS += \
+./appl.o 
+
+C_DEPS += \
+./appl.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o %.su: ../%.c subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F405xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/DH/Desktop/testled/LED_MODULE" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean--2e-
+
+clean--2e-:
+	-$(RM) ./appl.d ./appl.o ./appl.su
+
+.PHONY: clean--2e-
+
---
+++ Debug/xxx/appl.d
@@ -0,0 +1,4 @@
+xxx/appl.o: C:/Users/Administrator/Desktop/zx/xxx/appl.c \
+ C:/Users/Administrator/Desktop/zx/xxx/appl.h
+
+C:/Users/Administrator/Desktop/zx/xxx/appl.h:
